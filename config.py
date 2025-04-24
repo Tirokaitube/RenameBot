7d9dcb72e8377a36ad1c9c6516e2a36682ad8325
Tirokaitube <tirokaitube@gmail.com>
2025-04-24 14:09:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "https://i.ibb.co/PvHsTVXC/x.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5953621837').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "Anime_Lumino") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
+    LOG_CHANNEL = int(os.environ.get("-1002568229104", None))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
